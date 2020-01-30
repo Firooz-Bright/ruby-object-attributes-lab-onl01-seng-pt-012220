8fb7f7c86509e62a75994c73e2cfa6660bbbed4c
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2020-01-30 23:26:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,5 +2,17 @@ class Dog
   def initilaize(name, breed)
     @name= name 
     @breed= breed
-    
+  end 
+  def name=(name)
+    @name=name
+  end 
+  def breed=(breed)
+    @breed=breed 
+  end 
+  def name
+    @name
+  end 
+  def breed 
+    @breed
+  end 
 end 
