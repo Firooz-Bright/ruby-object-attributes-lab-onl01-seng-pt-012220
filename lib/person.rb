30cace58ff219ee1c92c99155ec35de1d39028b3
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2020-01-30 23:29:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,19 @@ class Person
   def initilaize(name,job)
     @name= name 
     @job=job
-    
+  end 
+  def name=(name)
+    @name= name
+  end 
+  def job=(job)
+    @job= job 
+  end 
+  def job
+    @job=job 
+  end 
+  def name
+    @name= name
+  end
 end 
 
   
